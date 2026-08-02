789ae92b610eb959d2ba39dee5bd75b01276b275
Nguyen The Truong <nguyenthetruong071105@gmail.com>
2026-08-02 16:25:56 +0700

    Nhúng file tĩnh vào dự án

--- index.js
+++ index.js
@@ -1,13 +1,27 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const path = require('path');
+
+// chỉ định thư mục chứa file giao diện
+app.set('views', path.join(__dirname, 'views'));
+
+// cấu hình view engine
+app.set('view engine', 'pug');
+
+// cấu hình thư mục file tĩnh
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
-  res.send('Trang chủ!');
+  res.render('client/pages/home', {
+    pageTitle: 'Trang chủ',
+  });
 });
 
 app.get('/tour', (req, res) => {
-  res.send('Danh sách tour!');
+  res.render('client/pages/tour', {
+    pageTitle: 'Tour',
+  });
 });
 
 app.listen(port, () => {
