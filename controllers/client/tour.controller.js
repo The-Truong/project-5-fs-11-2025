374b99bd93d33d99f5915c2288420598741bf7c8
Nguyen The Truong <nguyenthetruong071105@gmail.com>
2026-08-09 23:18:48 +0700

    tích hợp trang danh sách tour

--- controllers/client/tour.controller.js
+++ controllers/client/tour.controller.js
@@ -2,8 +2,8 @@ const Tour = require("../../models/tour.model");
 
 module.exports.list = async (req, res) => {
   const tourList = await Tour.find({});
-  res.render('client/pages/tour', {
-    pageTitle: 'Tour',
+  res.render('client/pages/tour-list', {
+    pageTitle: 'Danh sách tour',
     tourList: tourList,
   });
 }
