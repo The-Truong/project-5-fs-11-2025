8bdece32dcc9036331c4d6cdf1a55fd3a473e721
Nguyen The Truong <nguyenthetruong071105@gmail.com>
2026-08-10 22:49:46 +0700

    tích hợp quên mật khẩu, otp, đổi mật khẩu

--- routes/admin/accout.route.js
+++ routes/admin/accout.route.js
@@ -4,6 +4,13 @@ const accountController = require('../../controllers/admin/account.controller');
 const router = express.Router();
 
 router.get('/login', accountController.login);
+
 router.get('/register', accountController.register);
+
+router.get('/forgot-password', accountController.forgotPassword);
+
+router.get('/otp-password', accountController.otpPassword);
+
+router.get('/reset-password', accountController.resetPassword);
 
 module.exports = router;
