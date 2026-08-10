c882b02f2e1b0befbfc599e332b47fd219e6f13c
Nguyen The Truong <nguyenthetruong071105@gmail.com>
2026-08-10 23:03:19 +0700

    tích hợp trang tổng quan

--- routes/admin/index.route.js
+++ routes/admin/index.route.js
@@ -1,7 +1,10 @@
 const express = require('express');
-const accountRoutes = require('./accout.route');
+const accountRoutes = require('./accout.route'); 
+const dashboardRoutes = require('./dashboard.route'); 
 const router = express.Router();
 
 router.use('/account', accountRoutes);
+
+router.use('/dashboard', dashboardRoutes);
 
 module.exports = router;
