3efab0b230cdd9f0a95bd00db070670b02e0c6d1
Nguyen The Truong <nguyenthetruong071105@gmail.com>
2026-08-10 17:41:38 +0700

    sửa đường dẫn linh động

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ require('dotenv').config()
 const connectDB = require('./configs/database.config');
 const adminRoutes = require('./routes/admin/index.route');
 const clientRoutes = require('./routes/client/index.route');
-
+const { pathAdmin } = require('./configs/variable.config');
 const app = express();
 const port = 3000;
 
@@ -20,8 +20,11 @@ app.set('view engine', 'pug');
 // cấu hình thư mục file tĩnh
 app.use(express.static(path.join(__dirname, 'public')));
 
+// thêm biến toàn cục trong pug
+app.locals.pathAdmin = pathAdmin;
+
 //cấu hình đường dẫn
-app.use('/admin', adminRoutes);
+app.use(`/${pathAdmin}`, adminRoutes);
 app.use('/', clientRoutes);
 
 app.listen(port, () => {
