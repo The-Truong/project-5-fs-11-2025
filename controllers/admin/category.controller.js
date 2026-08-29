dcdafc780e451a521965429ff8ee503a7d5c944d
Nguyen The Truong <nguyenthetruong071105@gmail.com>
2026-08-29 16:57:48 +0700

    chọn danh mục cha

--- controllers/admin/category.controller.js
+++ controllers/admin/category.controller.js
@@ -1,4 +1,5 @@
 const Category = require("../../models/category.model");
+const { buildCategoryTree } = require("../../helpers/category.helper");
 
 module.exports.list = (req, res) => {
   res.render('admin/pages/category-list', {
@@ -6,9 +7,13 @@ module.exports.list = (req, res) => {
   });
 }
 
-module.exports.create = (req, res) => {
+module.exports.create = async (req, res) => {
+  const categoryList = await Category.find();
+  const categoryTree = buildCategoryTree(categoryList,"");
+
   res.render('admin/pages/category-create', {
     pageTitle: 'Tạo danh mục',
+    categoryList: categoryTree,
   });
 }
 
